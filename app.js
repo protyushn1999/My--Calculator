c75460ba48765f99c01a8a7febcadc97e4c6f831
Protyush Nayak <protyushn@gmail.com>
2021-11-10 12:57:53 +0530

    functionality added

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const res = document.querySelector(".res");
 const text = document.querySelector(".text");
 const button = document.querySelectorAll(".btn");
 
-
 var operand1 = 0;
 var operand2 = 0;
 var operator = "";
 
 // clear function reset the whole screen and set it to initial state
+
 function clear() {
   operand1 = 0;
   operand2 = 0;
@@ -18,110 +18,57 @@ function clear() {
   text.innerHTML = "&nbsp";
 }
 // deleteLast funnction => clears the last typed character from the screen
+
 function deleteLast() {
-   if(text.innerHTML.length > 1) {
+  if (text.innerHTML.length > 1) {
     const len = text.innerHTML.length;
     text.innerHTML = text.innerHTML.substring(0, len - 1);
     operand1 = text.innerHTML;
-   }
-    else {
-      text.innerHTML = "";
-    }
+  } else {
+    text.innerHTML = "";
+  }
 }
 
+// equalTo calculates the result and display it on the screen.
 
+function equalTo() {
+  text.innerHTML = result;
+  res.innerHTML = 0;
+  operand1 = text.innerHTML;
+  console.log(operand1);
+}
 
 for (let btn of button) {
   btn.addEventListener("click", function (e) {
-        console.log(btn.id);
-        if(btn.id === 'clc')
-        {
-            clear();
-            return;
-        }
+    // console.log(btn.id);
 
-        if(btn.id === 'del')
-        {
-            deleteLast();
-            return;
-        }
+    if (btn.id === "clc") {
+      clear();
+      return;
+    }
 
-        if(btn.id === '=')
-        {
-            text.innerHTML = result;
-            res.innerHTML =  0;
-            operand1 = text.innerHTML;
-            console.log(operand1);
-            return;
-        }
+    if (btn.id === "del") {
+      deleteLast();
+      return;
+    }
 
-        // if(btn.id === '.' && text.innerText.indexOf('.') !== -1)
-        // {
-        //     // if(text.innerText.indexOf('.') !== -1)
-        //     // {
-        //         const slicedText = text.innerText.slice(text.innerText.indexOf('.')+1);
-        //         if(slicedText.includes('.'))
-        //         {
-        //             return;
-        //         }
-        //         else {
-        //             text.innerText += btn.id;
-        //             return;
-        //         } 
-                
-            
-            
-        // }
+    if (btn.id === "=") {
+      equalTo();
+      return;
+    }
 
+    if(btn.id === "+" || btn.id === "-" || btn.id === "*" || btn.id === "/") {
+      text.innerText += btn.id;
+      operand1 = btn.id;
+    }
 
-        text.innerText += btn.id;
-        operand1 = (text.innerText);
-        result = eval(operand1).toFixed(5);
-        res.innerHTML = result;
+    else{
+      text.innerText += btn.id;
+      operand1 = text.innerText;
+    }
     
-        
-
-        
-
-        if(btn.id === '+' || btn.id === '-' || btn.id === '*' || btn.id === '/')
-        {
-            operator = btn.id;
-            operand1 = parseInt(res.innerHTML);
-            text.innertext = `operand1 ${operator}`;
-            return;
-        }
-        // if(btn.id === '.')
-        // {
-        //     if(btn.id === '.' && text.innerHTML.includes('.'))
-        //     {
-        //         const slicedText = y.slice(text.innerHTML.indexOf('.')+1);
-        //         if(slicedText.includes('.'))
-        //         {
-        //             return;
-        //         }
-                
-        //     }
-        //     else {
-        //         text.innerHTML += btn.id;
-        //     } 
-        //     return;  
-        // }
-
-
-        // const result = eval(`operand1 ${operator} operand2`);
-        // res.innerHTML = result;
-        
-
+      result = eval(operand1).toFixed(3);
+      res.innerHTML = result;
+    
   });
 }
-
-
-
-// function
-
-
-// calc function
-// render res
-// render typed values
-
-
